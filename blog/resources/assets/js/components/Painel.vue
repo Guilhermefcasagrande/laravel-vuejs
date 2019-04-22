9531d8227e3c31dc12886bea18cd98cb90167819
Guilhermefcasagrande <guilhermefranciscocasagrande@gmail.com>
2019-04-22 16:32:56 -0300

    Criado componente painel, e css persoalizado

---
+++ blog/resources/assets/js/components/Painel.vue
@@ -0,0 +1,32 @@
+<template>
+    <div v-bind:class="defineCor">
+        <div class="panel-heading">{{ titulo }}</div>
+
+        <div class="panel-body">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['titulo','cor'],
+        computed:{
+            defineCor: function(){
+                return "panel "+ (this.cor || "panel-primary");
+            }
+        }
+    }
+</script>
+
+<style>
+    .blue{
+        border-color: #081b89;
+    }
+    .blue > .panel-heading {
+        color: #fff;
+        background-color: #081b89;
+        border-color: #081b89;
+    }
+</style>
+
--- blog/resources/views/home.blade.php
+++ blog/resources/views/home.blade.php
@@ -2,6 +2,35 @@
 
 @section('content')
 
-    <example-component></example-component>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <painel titulo="Painel da Home" cor="panel-success">
+                    Teste de conteúdo do painel
+
+                    <div class="row">
+
+                        <div class="col-md-4">
+                            <painel titulo="Painel 1">
+                                Teste de componente dentro de componente
+                            </painel>
+                        </div>
+                        <div class="col-md-4">
+                            <painel titulo="Painel 2">
+                                Teste de componente dentro de componente
+                            </painel>
+                        </div>
+                        <div class="col-md-4">
+                            <painel titulo="Painel 3" cor="blue">
+                                Teste de componente dentro de componente
+                            </painel>
+                        </div>
+
+                    </div>
+
+                </painel>
+            </div>
+        </div>
+    </div>
 
 @endsection
